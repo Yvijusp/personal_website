f113ca4a83b421b8b965e98427e391e1e462b28a
Yvijusp <yvijus@hotmail.com>
2022-03-28 12:02:20 +0300

    mobile for projects

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import Button from '../components/button.svelte';
 	import ProjectCard from '../components/projectCard.svelte';
 	import { projects } from '../portfolio';
 </script>
 
 <section class="py-40 px-10 h-screen">
-	<div class="grid grid-cols-2 gap-4 max-w-5xl m-auto">
+	<div class="grid md:grid-cols-2 gap-4 max-w-5xl m-auto grid-cols-1">
 		{#each projects as project}
 			<ProjectCard {...project} />
 		{/each}
